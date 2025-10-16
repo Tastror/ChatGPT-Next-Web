a2aa5c0b40ba628d56c0599ef15e381a1de697f3
Tastror <yccozy2001@163.com>
2025-10-16 22:43:10 +0800

    just use JSON.stringify

--- app/api/google.ts
+++ app/api/google.ts
@@ -159,8 +159,7 @@ async function request(req: NextRequest, apiKey: string) {
   
       if (!res.ok) {
         const errorBody = await res.text();
-        const errorEventRaw = `data: ${JSON.stringify({ error: true, message: `Upstream Error: ${res.status} ${res.statusText}`, body: errorBody })}\n\n`;
-        const errorEvent = errorEventRaw.replaceAll("`", "\\`").replaceAll("$", "\\$").replaceAll("\n", "\\n");
+        const errorEvent = JSON.stringify({ error: true, message: `Upstream Error: ${res.status} ${res.statusText}`, body: errorBody }, null, 2);
         writer.write(encoder.encode(`data: {"candidates":[{"content":{"role":"model","parts":[{"text":"\\n\\n\`\`\`json\\n${errorEvent}\\n\`\`\`"}]},"finishReason":null,"index":0,"safetyRatings":[]}],"promptFeedback":{"safetyRatings":[]}}\n\n`));
         throw new Error(`[Alive] Upstream fetch failed with status ${res.status}`);
       }
